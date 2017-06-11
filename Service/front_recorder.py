4eb03110beb3d731ae39adfe22ecc4327312f9d3
Your Name <you@example.com>
2017-06-11 06:36:35 +0000

    add python scripts

---
+++ Service/front_recorder.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import subprocess
+import datetime
+
+now = datetime.datetime.now()
+filename = now.strftime("%Y%m%d_%H%M%S")
+
+subprocess.call("/opt/vc/bin/raspivid -w 960 -h 540 -n -hf -vf -o /home/camera/front/record{0}.h264 -t 10000".format(filename).split())
+#subprocess.call("/opt/vc/bin/raspivid -w 960 -h 540 -n -hf -vf -o /home/camera/front/record{0}.h264 -t 1000000".format(filename).split())
