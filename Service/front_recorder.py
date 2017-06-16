33cbe421c026d436e009ad15b6b3b3b1122b123f
Your Name <you@example.com>
2017-06-16 12:05:06 +0000

    disk space

--- Service/front_recorder.py
+++ Service/front_recorder.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python
 
+import os
 import subprocess
-import datetime
 
-now = datetime.datetime.now()
-filename = now.strftime("%Y%m%d_%H%M%S")
 
-subprocess.call("/opt/vc/bin/raspivid -w 960 -h 540 -n -hf -vf -o /home/camera/front/record{0}.h264 -t 1000000".format(filename).split())
+# Make new file name
+all_files = os.listdir('/home/camera/front/')
+current_numbers = []
+for file_name in all_files:
+    current_numbers.append(int(file_name[:-5]))
+current_numbers.sort()
+if (current_numbers):
+    next_number = current_numbers[-1] + 1
+else:
+    next_number = 0
+new_filename = "{}".format(next_number)
+
+# Check disk space
+if (current_numbers):
+    remove_number = current_numbers[0]
+    p = subprocess.Popen("df -h".split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
+    disk_space = int(p.stdout.readlines()[1].split()[4][:-1])
+    if (80 < disk_space):
+        subprocess.call("rm /home/camera/front/{}.h264".format(remove_number).split())
+
+subprocess.call("/opt/vc/bin/raspivid -w 960 -h 540 -n -hf -vf -o /home/camera/front/{0}.h264 -t 1000000".format(new_filename).split())
--- Service/rear_recorder.py
+++ Service/rear_recorder.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 import subprocess
-import datetime
+#import datetime
 
-now = datetime.datetime.now()
-dirname = now.strftime("%Y%m%d_%H%M%S")
+#now = datetime.datetime.now()
+#dirname = now.strftime("%Y%m%d_%H%M%S")
 
-subprocess.call("mkdir /home/camera/rear/{}".format(dirname).split())
-subprocess.call("motion".split(), cwd="/home/camera/rear/{}".format(dirname))
+#subprocess.call("mkdir /home/camera/rear/{}".format(dirname).split())
+subprocess.call("motion".split(), cwd="/home/camera/rear/")
