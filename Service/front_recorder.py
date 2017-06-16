308a6570b985aa250b072733f052b61bd827370c
Your Name <you@example.com>
2017-06-16 09:49:37 +0000

    update time

--- Service/front_recorder.py
+++ Service/front_recorder.py
@@ -6,5 +6,4 @@ import datetime
 now = datetime.datetime.now()
 filename = now.strftime("%Y%m%d_%H%M%S")
 
-subprocess.call("/opt/vc/bin/raspivid -w 960 -h 540 -n -hf -vf -o /home/camera/front/record{0}.h264 -t 10000".format(filename).split())
-#subprocess.call("/opt/vc/bin/raspivid -w 960 -h 540 -n -hf -vf -o /home/camera/front/record{0}.h264 -t 1000000".format(filename).split())
+subprocess.call("/opt/vc/bin/raspivid -w 960 -h 540 -n -hf -vf -o /home/camera/front/record{0}.h264 -t 1000000".format(filename).split())
